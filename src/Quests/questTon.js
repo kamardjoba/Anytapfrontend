fcd31f8758bf40fb1f5394ded401b5b5f0ae072a
kamardjoba <Nazarlymar152@gmail.com>
2024-08-25 00:35:34 +0200

    initial

--- src/Quests/questTon.js
+++ src/Quests/questTon.js
@@ -1,54 +1,67 @@
-import React from 'react';
+import React, {useEffect}from 'react';
 import '../Css/Quests.css';
-import arrows from '../IMG/arrows.svg';
+import arrows from'../IMG/arrows.svg';
+import { TonConnectUIProvider, TonConnectButton} from '@tonconnect/ui-react';
 import { useTonConnectUI } from '@tonconnect/ui-react';
 
-const TonTrans = ({ TonTran_val }) => {
-    const tonConnectContext = useTonConnectUI();
-    console.log('TonConnect Context:', tonConnectContext);
 
-    const { sendTransaction } = tonConnectContext;
 
-    const GoTon = async () => {
+const TonTrans = ({GoTon, TonTran_val}) => {
+    const [tonConnectUI] = useTonConnectUI();
+    useEffect(() => {
+        if (window.TON_CONNECT_UI) {
+            const tonConnectUI = new window.TON_CONNECT_UI.TonConnectUI({
+                manifestUrl: 'https://resilient-madeleine-9ff7c2.netlify.app/tonconnect-manifest.json',
+                buttonRootId: 'TonMainConBtn'
+            });
+    
+            tonConnectUI.onStatusChange((walletInfo) => {
+                if (walletInfo) {
+                    console.log('Кошелек подключен!', walletInfo);
+                } else {
+                    console.log('Кошелек отключен!');
+                }
+            });
+        }
+    }, []);
+     GoTon = async () => {
         try {
-            if (!sendTransaction) {
-                throw new Error("sendTransaction is not available");
-            }
-
             const transaction = {
                 validUntil: Date.now() + 5 * 60 * 1000,
                 messages: [
                     {
-                        address: 'EQD6p7QkZK1npse29QdZ5ehTo1o3nEXW9fb-5aHkKX5gTonConnectAddress',
-                        amount: '10000', // 0.01 TON в нанотонах
+                        address: 'UQC-ZK_dPpZ15VaL-kwyXT1jTCYDTQricz8RxvXT0VmdbRYG',
+                        amount: '1000000', // 1 TON в нанотонах
                     },
                 ],
             };
 
-            await sendTransaction(transaction);
+            await tonConnectUI.sendTransaction(transaction);
             alert('Transaction sent successfully!');
         } catch (error) {
-            console.error('Transaction failed', error);
+            console.error('Transaction failed:', error);
             alert('Transaction failed: ' + error.message);
         }
     };
+  return (
+    <TonConnectUIProvider manifestUrl="https://resilient-madeleine-9ff7c2.netlify.app/tonconnect-manifest.json">
 
-    return (
-        <div className='questItem'>
-            <div className='questItemLeft'>
-                <div className='questIcon'>
-                    <img src={arrows} alt=""/>
-                </div>
-                <div className='questItemLeftContent'>
-                    <p className='questTitle'>Make Ton transactions</p>
-                    <p className='questSubtitle'>+5000 Points</p>
-                </div>
+    <div className='questItem'>
+        <div className='questItemLeft'>
+            <div className='questIcon'>
+                <img src={arrows} alt=""/>
             </div>
-            <div className='questItemRight'>
-                {!TonTran_val && (<button className='questBtn' onClick={GoTon}>GO!</button>)}
+            <div className='questItemLeftContent'>
+                <p className='questTitle'>Make Ton transactions</p>
+                <p className='questSubtitle'>+5000 Points</p>
             </div>
         </div>
-    );
+        <div className='questItemRight'>
+        {!TonTran_val && (<button className='questBtn' onClick={GoTon}>GO!</button>)}
+        </div>
+    </div>
+    </TonConnectUIProvider>
+  );
 };
 
 export default TonTrans;
