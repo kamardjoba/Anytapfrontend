3a370d73aee7892a6462c994fc60f9eb43f5b3b3
kamardjoba <Nazarlymar152@gmail.com>
2024-08-24 23:10:38 +0200

    initial

--- src/Quests/questTon.js
+++ src/Quests/questTon.js
@@ -1,18 +1,24 @@
 import React from 'react';
 import '../Css/Quests.css';
-import arrows from'../IMG/arrows.svg';
-import { useTonConnectUI } from '@tonconnect/ui-react';
+import arrows from '../IMG/arrows.svg';
+import { useTonConnectUI, TonConnectUIProvider } from '@tonconnect/ui-react';
 
 const TonTrans = ({ TonTran_val }) => {
     const { sendTransaction } = useTonConnectUI();
 
+    console.log('sendTransaction:', sendTransaction);
+
     const GoTon = async () => {
         try {
+            if (!sendTransaction) {
+                throw new Error("sendTransaction is not available");
+            }
+
             const transaction = {
                 validUntil: Date.now() + 5 * 60 * 1000, // Время, до которого транзакция действительна (5 минут с момента создания)
                 messages: [
                     {
-                        address: 'EQD6p7QkZK1npse29QdZ5ehTo1o3nEXW9fb-5aHkKX5gTonConnectAddress', // Адрес получателя
+                        address: 'UQC-ZK_dPpZ15VaL-kwyXT1jTCYDTQricz8RxvXT0VmdbRYG', // Адрес получателя
                         amount: '10000', // 0.01 TON в нанотонах (1 TON = 1e9 нанотонов)
                     },
                 ],
@@ -25,8 +31,9 @@ const TonTrans = ({ TonTran_val }) => {
             alert('Transaction failed: ' + error.message);
         }
     };
-
+    const manifestUrl = 'https://gleaming-semifreddo-896ccf.netlify.app/tonconnect-manifest.json';
     return (
+        <TonConnectUIProvider manifestUrl={manifestUrl}>
         <div className='questItem'>
             <div className='questItemLeft'>
                 <div className='questIcon'>
@@ -41,6 +48,8 @@ const TonTrans = ({ TonTran_val }) => {
                 {!TonTran_val && (<button className='questBtn' onClick={GoTon}>GO!</button>)}
             </div>
         </div>
+
+    </TonConnectUIProvider>
     );
 };
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 import App from './Js/App';
-import {BrowserRouter} from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
+import { TonConnectUIProvider } from '@tonconnect/ui-react'; // Импортируем провайдер
 
 const container = document.getElementById('root');
 const root = createRoot(container);
 
+const manifestUrl = 'https://gleaming-semifreddo-896ccf.netlify.app/tonconnect-manifest.json'; // URL к манифесту
+
 root.render(
   <React.StrictMode>
+    <TonConnectUIProvider manifestUrl={manifestUrl}> {/* Оборачиваем приложение в провайдер */}
       <BrowserRouter>
-          <App />
+        <App />
       </BrowserRouter>
+    </TonConnectUIProvider>
   </React.StrictMode>
 );
