447d0cf9d11a86e0da4e1ecfc78a0d76b5a6a60c
kamardjoba <Nazarlymar152@gmail.com>
2024-08-24 21:22:04 +0200

    initial

---
+++ src/Js/TonConnect.js
@@ -0,0 +1,29 @@
+import { TonConnect } from '@tonconnect/sdk';
+
+const connector = new TonConnect({
+    manifestUrl: 'https://gleaming-semifreddo-896ccf.netlify.app/tonconnect-manifest.json',
+});
+
+export const connectWallet = async () => {
+    try {
+        await connector.restoreConnection();
+        if (!connector.connected) {
+            await connector.connectWallet();
+        }
+        return connector.wallet;
+    } catch (error) {
+        console.error("Failed to connect wallet:", error);
+        return null;
+    }
+};
+
+export const disconnectWallet = () => {
+    connector.disconnect();
+};
+
+export const getWalletAddress = () => {
+    return connector.connected ? connector.wallet?.account.address : null;
+};
+
+console.log("TonConnect.js loaded");
+console.log("connectWallet function:", typeof connectWallet);
