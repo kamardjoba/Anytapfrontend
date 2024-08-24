2b6ce71a6cafdde6c4afb8a153310f87a15e177c
kamardjoba <Nazarlymar152@gmail.com>
2024-08-24 21:33:29 +0200

    initial

--- src/Js/TonConnect.js
+++ src/Js/TonConnect.js
@@ -3,13 +3,20 @@ import { TonConnect } from '@tonconnect/sdk';
 const connector = new TonConnect({
     manifestUrl: 'https://gleaming-semifreddo-896ccf.netlify.app/tonconnect-manifest.json',
 });
+console.log("Connector object:", connector);
+console.log("Connector methods:", Object.keys(connector));
 
 export const connectWallet = async () => {
     try {
         await connector.restoreConnection();
+        console.log("Connector connected:", connector.connected);
+
+        // Проверьте, есть ли другой метод для подключения кошелька
         if (!connector.connected) {
-            await connector.connectWallet();
+            const wallet = await connector.requestSign();
+            console.log("Wallet request sign:", wallet);
         }
+
         return connector.wallet;
     } catch (error) {
         console.error("Failed to connect wallet:", error);
