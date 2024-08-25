808766865e6a8cc096fa464d9e6bcad511c544ea
Aferist163 <aferist.game.163@gmail.com>
2024-08-25 19:22:48 +0200

    innitial

--- src/Quests/questTonWallet.js
+++ src/Quests/questTonWallet.js
@@ -33,15 +33,17 @@ const TonW = ({ Wallet_val, telegramId, wallet }) => {
   return (
     <TonConnectUIProvider manifestUrl="https://gleaming-semifreddo-896ccf.netlify.app/tonconnect-manifest.json">
     <div className='questItem'>
-        <div className='questIcon'>
-            <img src={wallet} alt=""/>
-        </div>
-        <div className='questItemLeftContent'>
-            <p className='questTitle'>Ton Wallet Connect</p>
-            <p className='questSubtitle'>+500 points</p>
+        <div className='questItemLeft'>
+            <div className='questIcon'>
+                <img src={wallet} alt=""/>
+            </div>
+            <div className='questItemLeftContent'>
+                <p className='questTitle'>Ton Wallet Connect</p>
+                <p className='questSubtitle'>+500 points</p>
+            </div>
         </div>
         <div className='questItemRight'>
-        {!Wallet_val && (<TonConnectButton/>)}
+            {!Wallet_val &&(<TonConnectButton/>)}
         </div>
     </div>
     </TonConnectUIProvider>
