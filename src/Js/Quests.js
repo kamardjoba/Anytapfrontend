f701ec66d56e88485a4a151faa5370629d29fcc0
Aferist163 <aferist.game.163@gmail.com>
2024-08-27 01:51:32 +0200

    innitial

--- src/Js/Quests.js
+++ src/Js/Quests.js
@@ -222,7 +222,7 @@ function Quests({ X, arrows, invite, MintStart, wallet, inst, telegram,
                 {!Wallet_val && <TonW Wallet_val={Wallet_val} wallet={wallet} telegramId={telegramId} />}
                 {!TgChanel_val && <TgQuest GoTg={GoTg} telegram={telegram} />}
                 {!TgOcties_val && <TgOctiesQuest GoOct={GoOct} telegram={telegram}/>}
-                {!X_val && <XQuest GoX={GoX} X={X} setXVal={setXVal}/>}
+                {!X_val && <XQuest GoX={GoX} X={X}/>}
                 {!Inst_val && <InstQuest GoInst={GoInst} inst={inst}/>}
                 {!StartNft_val && <MintStartNft  StartNft_val={StartNft_val}  MintStart={MintStart} telegramId={telegramId}/>}
                 {!Frends_val && <FrendsQuest telegramId={telegramId} invite={invite} referralsCount={referralsCount} />}
