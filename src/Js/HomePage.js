3603f1258a60c53d06688e281efabaed3f2895a6
kamardjoba <Nazarlymar152@gmail.com>
2024-08-26 21:55:39 +0200

    initial

--- src/Js/HomePage.js
+++ src/Js/HomePage.js
@@ -1,36 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import '../Css/HomePage.css';
 import diamond from '../IMG/diamond.png';
-const [userCoins, setUserCoins] = useState(null);
 
 function HomePage(props) {
+    const [userCoins, setUserCoins] = useState(null);
 
-    const telegramId = urlParams.get('telegramId');
-    if (telegramId) {
-        fetch(`https://anypatbackend-production.up.railway.app/user-info?telegramId=${telegramId}`)
-            .then(response => response.json())
-            .then(data => {
-                if (data.success) {
-                    setUserCoins = data.coins;
-                    setLoading(false);
-                } else {
-                    console.error('Ошибка при получении данных о пользователе:', data.message);
-                    setLoading(false);
-                }
-            })
-            .catch(error => {
-                console.error('Ошибка при запросе:', error);
-                setLoading(false);
-            });
-    }
+    useEffect(() => {
+        const urlParams = new URLSearchParams(window.location.search);
+        const telegramId = urlParams.get('telegramId');
+
+        if (telegramId) {
+            fetch(`https://anypatbackend-production.up.railway.app/user-info?telegramId=${telegramId}`)
+                .then(response => response.json())
+                .then(data => {
+                    if (data.success) {
+                        setUserCoins(data.coins);
+                    } 
+                })
+                .catch(error => {
+                    console.error('Error fetching user info:', error);
+                });
+        }
+    }, []);
 
     return (
         <div className='homepageContainer'>
             <div className='homepageWrapper'>
                 <p className='welcomeText'>Welcome to <br/>Anytap!</p>
                 <div className='amountWrapper'>
-                    <p className='amount'>{coins}</p>
-                    <img src={diamond} alt="" className='diamondImgHomepage'  onClick={(event) => {localStorage.clear();}}/>
+                    <p className='amount'>{userCoins}</p>
+                    <img src={diamond} alt="" className='diamondImgHomepage' onClick={(event) => {localStorage.clear();}}/>
                 </div>
                 <p className='homepageDescr'>
                     Earn points, mint NFTs, <br/>and receive valuable <br/>  rewards for your activity!
