6a347878a32ea541b001832db87b6bfddf50f9e7
Aferist163 <aferist.game.163@gmail.com>
2024-08-28 20:29:45 +0200

    innitial

--- src/Js/Leaderboard.js
+++ src/Js/Leaderboard.js
@@ -149,15 +149,17 @@ function Leaderboard() {
                                 <div class="loaderBlue"></div>
                             </div>)}
 
-                        {isLoadingLiderBlue  ? (
-                            <div className='blueContainerItem'>
-                                <p className='blueContainerItemTitle'>{totalUsers ? totalUsers.toLocaleString() : 'Loading...'}</p>
-                                <p className='blueContainerItemSubtitle'>Total users</p>
-                            </div>
-                        ) : (
-                            <div className={`blueContainerItem ${isLoadingLiderBlueSupport ? 'hiddenMain' : ''}`}>
-                                <div className="loaderBlue"></div>
-                            </div>)}
+                            {isLoadingLiderBlue ? (
+    <div className={`blueContainerItem fadeIn`}>
+        <p className='blueContainerItemTitle'>{totalUsers ? totalUsers.toLocaleString() : 'Loading...'}</p>
+        <p className='blueContainerItemSubtitle'>Total users</p>
+    </div>
+) : (
+    <div className={`blueContainerItem ${isLoadingLiderBlueSupport ? 'hiddenMain' : 'fadeIn'}`}>
+        <div className="loaderBlue"></div>
+    </div>
+)}
+
                     </div>
                 </>)}
 
