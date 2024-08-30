d7aa7f9a95990e7b58f1a901e2aeebf98d49649f
Aferist163 <aferist.game.163@gmail.com>
2024-08-30 21:18:11 +0200

    innitial

--- src/Quests/questTonWallet.js
+++ src/Quests/questTonWallet.js
@@ -15,25 +15,25 @@ const TonW = ({ telegramId, wallet }) => {
                
 
                 // Отправляем запрос на сервер для добавления 500 монет пользователю
-                try {
-                    const response = await axios.post('https://anypatbackend-production.up.railway.app/wallet-connected', { telegramId });
-                    if (response.data.success) {
-                        console.log('500 монет добавлено пользователю');
+                // try {
+                //     const response = await axios.post('https://anypatbackend-production.up.railway.app/wallet-connected', { telegramId });
+                //     if (response.data.success) {
+                //         console.log('500 монет добавлено пользователю');
 
-                        // Теперь отправляем запрос на обновление монет у реферера
-                        const referralUpdateResponse = await axios.post('https://anypatbackend-production.up.railway.app/add-coins-to-referral', { telegramId, amount: 500 });
-                        if (referralUpdateResponse.data.success) {
-                            console.log('Монеты реферера обновлены');
-                        } else {
-                            console.error('Ошибка при обновлении монет реферера:', referralUpdateResponse.data.message);
-                        }
+                //         // Теперь отправляем запрос на обновление монет у реферера
+                //         const referralUpdateResponse = await axios.post('https://anypatbackend-production.up.railway.app/add-coins-to-referral', { telegramId, amount: 500 });
+                //         if (referralUpdateResponse.data.success) {
+                //             console.log('Монеты реферера обновлены');
+                //         } else {
+                //             console.error('Ошибка при обновлении монет реферера:', referralUpdateResponse.data.message);
+                //         }
 
-                    } else {
-                        console.error('Ошибка при начислении монет:', response.data.message);
-                    }
-                } catch (error) {
-                    console.error('Ошибка при добавлении монет:', error);
-                }
+                //     } else {
+                //         console.error('Ошибка при начислении монет:', response.data.message);
+                //     }
+                // } catch (error) {
+                //     console.error('Ошибка при добавлении монет:', error);
+                // }
             }
         });
     }
@@ -48,7 +48,6 @@ const TonW = ({ telegramId, wallet }) => {
               </div>
               <div className='questItemLeftContent'>
                   <p className='questTitle'>Wallet Connect</p>
-                  <p className='questSubtitle'>+500 points</p>
               </div>
           </div>
           <div className='questItemRight'>
