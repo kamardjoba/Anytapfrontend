a849881267789be0f144b54a3fffac237ac2e146
kamardjoba <Nazarlymar152@gmail.com>
2024-09-02 20:05:30 +0200

    initial

--- src/Quests/AdsGramTask.js
+++ src/Quests/AdsGramTask.js
@@ -3,56 +3,58 @@ import React from 'react';
 import '../Css/Quests.css';
 
 const AdsGramTask = ({Ad, telegramId}) => {
-        const showAd = () => {
-            console.log("Кнопка Watch нажата");
-            if (!window.AdsGram) {
+    const showAd = () => {
+        console.log("Кнопка Watch нажата");
+    
+        // Откладываем выполнение на 2 секунды, чтобы дать время на загрузку AdsGram SDK
+        setTimeout(() => {
+            if (window.AdsGram) {
+                console.log("Инициализация AdsGram");
+                window.AdsGram.AdController.show({
+                    onAdClosed: async () => {
+                        console.log("Реклама закрыта");
+    
+                        try {
+                            const response = await fetch('https://anypatbackend-production.up.railway.app/add-coins', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify({ telegramId, amount: 200 }),
+                            });
+    
+                            const data = await response.json();
+                            if (data.success) {
+                                console.log('200 монет успешно добавлены пользователю');
+                            } else {
+                                console.error('Ошибка при добавлении монет пользователю:', data.message);
+                            }
+    
+                            const referralUpdateResponse = await fetch('https://anypatbackend-production.up.railway.app/add-coins-to-referral', {
+                                method: 'POST',
+                                headers: {
+                                    'Content-Type': 'application/json',
+                                },
+                                body: JSON.stringify({ telegramId, amount: 200 }),
+                            });
+    
+                            const referralData = await referralUpdateResponse.json();
+                            if (referralData.success) {
+                                console.log('Монеты реферера успешно обновлены');
+                            } else {
+                                console.error('Ошибка при обновлении монет реферера:', referralData.message);
+                            }
+    
+                        } catch (error) {
+                            console.error('Ошибка при обновлении монет:', error);
+                        }
+                    },
+                });
+            } else {
                 console.error('AdsGram SDK не загружен');
-                return;
             }
-    
-            console.log("Инициализация AdsGram");
-            window.AdsGram.AdController.show({
-                onAdClosed: async () => {
-                    console.log("Реклама закрыта");
-    
-                    try {
-                        const response = await fetch('https://anypatbackend-production.up.railway.app/add-coins', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({ telegramId, amount: 200 }),
-                        });
-    
-                        const data = await response.json();
-                        if (data.success) {
-                            console.log('200 монет успешно добавлены пользователю');
-                        } else {
-                            console.error('Ошибка при добавлении монет пользователю:', data.message);
-                        }
-    
-                        const referralUpdateResponse = await fetch('https://anypatbackend-production.up.railway.app/add-coins-to-referral', {
-                            method: 'POST',
-                            headers: {
-                                'Content-Type': 'application/json',
-                            },
-                            body: JSON.stringify({ telegramId, amount: 200 }),
-                        });
-    
-                        const referralData = await referralUpdateResponse.json();
-                        if (referralData.success) {
-                            console.log('Монеты реферера успешно обновлены');
-                        } else {
-                            console.error('Ошибка при обновлении монет реферера:', referralData.message);
-                        }
-    
-                    } catch (error) {
-                        console.error('Ошибка при обновлении монет:', error);
-                    }
-                },
-            });
-        };
-
+        }, 2000); // Задержка на 2 секунды
+    };
     return (
         <div className='questItem'>
         <div className='questItemLeft'>
