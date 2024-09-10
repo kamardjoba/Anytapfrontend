f2e0adcde7ed4ff54ae50ad55e76cf49030e6292
kamardjoba <Nazarlymar152@gmail.com>
2024-09-10 15:16:58 +0200

    initial

--- src/Js/App.js
+++ src/Js/App.js
@@ -51,7 +51,7 @@ function App() {
     const [activeItem, setActiveItem] = useState(null);
 
     const [showLoading, setShowLoading] = useState(true);
-    const [isLoading, setLoading] = useState(false);
+    const [isLoading, setLoading] = useState(true);
 
     useEffect(() => {
         const handleStorageChange = () => {
