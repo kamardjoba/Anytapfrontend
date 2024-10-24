31fe9aa7eddddd9e43fdf33aa3cef88b2d026746
kamardjoba <Nazarlymar152@gmail.com>
2024-10-24 14:27:28 +0200

    initial

--- src/Js/Quests.js
+++ src/Js/Quests.js
@@ -174,6 +174,14 @@ function Quests({ X, arrows, invite, MintStart, wallet, inst, Ad, telegram,
                             localStorage.setItem('GoCaptcha_val', 'false'); 
                             window.dispatchEvent(new Event('storage'));
                         }
+                        if (response.data.isSubscribedTofuccdafomo) {
+                            localStorage.setItem('Partners_val', 'true'); 
+                            window.dispatchEvent(new Event('storage'));
+                        }else{
+                            localStorage.setItem('Partners_val', 'false'); 
+                            window.dispatchEvent(new Event('storage'));
+                        }
+                        
 
                     } catch (error) {
                         console.error('Ошибка при проверке подписки:', error);
@@ -252,7 +260,7 @@ function Quests({ X, arrows, invite, MintStart, wallet, inst, Ad, telegram,
                 const telegramId = user.id;
                 setTimeout(async () => {
                     try {
-                        await axios.post('https://anypatbackend-production.up.railway.app/update-telegram-Chat', { telegramId });
+                        await axios.post('https://anypatbackend-production.up.railway.app/update-telegram-fuccdafomo', { telegramId });
                         localStorage.setItem('Partners_val', 'true'); 
                         window.dispatchEvent(new Event('storage'));
                         
